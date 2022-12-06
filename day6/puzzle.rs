4bb591acae9653c1822cc2ddbeb54c4d3b629336
Ashley Anderson <aganders3@gmail.com>
2022-12-06 13:21:45 -0500

    Day 6 in Rust

---
+++ day6/puzzle.rs
@@ -0,0 +1,35 @@
+use std::collections::HashSet;
+
+fn find_marker(input: &str, marker_len: usize) -> usize {
+    let mut unique = HashSet::new();
+    marker_len
+        + input
+            .chars()
+            .collect::<Vec<_>>()
+            .windows(marker_len)
+            .take_while(|x| {
+                unique.clear();
+                !x.into_iter().all(|y| unique.insert(y))
+            })
+            .count()
+}
+
+fn main() {
+    // part 1
+    assert!(find_marker("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 4) == 7);
+    assert!(find_marker("bvwbjplbgvbhsrlpgdmjqwftvncz", 4) == 5);
+    assert!(find_marker("nppdvjthqldpwncqszvftbrmjlhg", 4) == 6);
+    assert!(find_marker("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 4) == 10);
+    assert!(find_marker("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 4) == 11);
+    let n = find_marker(include_str!("input.txt"), 4);
+    println!("Part 1: {n}");
+
+    // part 2
+    assert!(find_marker("mjqjpqmgbljsphdztnvjfqwrcgsmlb", 14) == 19);
+    assert!(find_marker("bvwbjplbgvbhsrlpgdmjqwftvncz", 14) == 23);
+    assert!(find_marker("nppdvjthqldpwncqszvftbrmjlhg", 14) == 23);
+    assert!(find_marker("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", 14) == 29);
+    assert!(find_marker("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", 14) == 26);
+    let n = find_marker(include_str!("input.txt"), 14);
+    println!("Part 2: {n}");
+}
