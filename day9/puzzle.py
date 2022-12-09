0b4b80462e781ac28438beea7a63d9cd151320d9
Ashley Anderson <aganders3@gmail.com>
2022-12-09 13:38:43 -0500

    Day 9 Part 2

--- day9/puzzle.py
+++ day9/puzzle.py
@@ -24,13 +24,6 @@ class Rope:
         self.head = (self.head[0] + dx, self.head[1] + dy)
 
     def _move_tail(self) -> None:
-        # head and tail are already touching, do nothing
-        # if (
-        #     abs(self.head[0] - self.tail[0]) < 2
-        #     and abs(self.head[1] - self.tail[1]) < 2
-        # ):
-        #     pass
-
         dx = self.head[0] - self.tail[0]
         dy = self.head[1] - self.tail[1]
 
@@ -47,9 +40,60 @@ class Rope:
 
         self.visited_by_tail.add(self.tail)
 
+class LongRope:
+    def __init__(self, length: int = 10):
+        self.knots: list[tuple[int, int]] = [(0, 0)] * length
+        self.visited_by_tail: set[tuple[int, int]] = { self.tail }
+
+    def move_head(self, direction: str, n: int) -> None:
+        for _ in range(n):
+            self._move_head(direction)
+            self._move_tail()
+
+    @property
+    def head(self):
+        return self.knots[0]
+
+    @head.setter
+    def head(self, val):
+        self.knots[0] = val
+
+    @property
+    def tail(self):
+        return self.knots[-1]
+
+    def _move_head(self, direction: str) -> None:
+        dx, dy = DIRECTION[direction]
+        self.head = (self.head[0] + dx, self.head[1] + dy)
+
+    def _move_tail(self) -> None:
+        for i, knot in enumerate(self.knots[1:], start=1):
+            dx = self.knots[i - 1][0] - self.knots[i][0]
+            dy = self.knots[i - 1][1] - self.knots[i][1]
+
+            if 0 in (dx, dy):
+                self.knots[i] = (
+                    self.knots[i][0] + int(copysign(abs(dx) // 2, dx)),
+                    self.knots[i][1] + int(copysign(abs(dy) // 2, dy)),
+                )
+            elif 2 in (abs(dx), abs(dy)):
+                self.knots[i] = (
+                    self.knots[i][0] + int(copysign(max(1, abs(dx) // 2), dx)),
+                    self.knots[i][1] + int(copysign(max(1, abs(dy) // 2), dy)),
+                )
+
+        self.visited_by_tail.add(self.tail)
+
 
 def part_1(input_filename: str) -> int:
     rope = Rope()
+    for move in open(input_filename):
+        direction, n = move.strip().split(" ")
+        rope.move_head(direction, int(n))
+    return len(rope.visited_by_tail)
+
+def part_2(input_filename: str) -> int:
+    rope = LongRope()
     for move in open(input_filename):
         direction, n = move.strip().split(" ")
         rope.move_head(direction, int(n))
@@ -59,7 +103,10 @@ def part_1(input_filename: str) -> int:
 def test_part_1():
     assert part_1("test.input.txt") == 13
 
+def test_part_2():
+    assert part_2("test.input.2.txt") == 36
+
 
 if __name__ == "__main__":
     print("Part 1:", part_1("input.txt"))
-    # print("Part 1:", part_1("input.txt"))
+    print("Part 2:", part_2("input.txt"))
