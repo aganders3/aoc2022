8419b003c3df49ac25b099cb864f2f546ee05a3f
Ashley Anderson <aganders3@gmail.com>
2022-12-21 20:23:14 -0500

    Day 18 - Part 2

--- day18/puzzle.rs
+++ day18/puzzle.rs
@@ -73,11 +73,80 @@ fn part_1(s: &str) -> usize {
         }
     }
 
-    exposed_faces
+    dbg!(exposed_faces)
 }
 
+fn part_2(s: &str) -> usize {
+    let cubes = s.lines().map(Cube::parse).collect::<HashSet<_>>();
+    let mut outside: HashSet<Cube> = HashSet::new();
+
+    let x = cubes.iter().filter_map(|c| Some(c.x));
+    let min_x = x.clone().min().unwrap();
+    let max_x = x.max().unwrap();
+
+    let y = cubes.iter().filter_map(|c| Some(c.y));
+    let min_y = y.clone().min().unwrap();
+    let max_y = y.max().unwrap();
+
+    let z = cubes.iter().filter_map(|c| Some(c.z));
+    let min_z = z.clone().min().unwrap();
+    let max_z = z.max().unwrap();
+
+    // use a closure to capture the above values
+    let in_bounds = |cube: &Cube| -> bool {
+        (min_x - 1..=max_x + 1).contains(&cube.x)
+        && (min_y - 1..=max_y + 1).contains(&cube.y)
+        && (min_z - 1..=max_z + 1).contains(&cube.z)
+    };
+
+    let mut exposed_faces = 0;
+
+    cubes.iter().for_each(|cube| {
+        // neighbors are adjacent to the faces of each cube - this is what we're trying to count
+        cube.neighbors().iter().for_each(|n| {
+            if outside.contains(&n) {
+                // we already know this face is exposed!
+                exposed_faces += 1;
+            } else if !cubes.contains(&n) {
+                // BFS to try to get to outside
+                let mut to_visit = VecDeque::from([vec![*n]]);
+                let mut seen: HashSet<Cube> = HashSet::new();
+                let mut exposed = false;
+                while !to_visit.is_empty() {
+                    let cur = to_visit.pop_front().expect("we just checked...");
+                    for c in cur.last().unwrap().neighbors() {
+                        // don't backtrack and only follow open space
+                        if seen.contains(&c) || cubes.contains(&c) {
+                            continue;
+                        }
+
+                        // known to be outside
+                        if outside.contains(&c) || !in_bounds(&c) {
+                            exposed_faces += 1;
+                            outside.extend(cur.iter().cloned());
+                            outside.insert(c);
+                            exposed = true;
+                            break;
+                        }
+
+                        seen.insert(c);
+                        to_visit.push_back(
+                            cur.iter().chain([&c]).cloned().collect()
+                        );
+                    }
+                    if exposed { break; }
+                }
+            }
+        });
+    });
+
+    dbg!(exposed_faces)
+}
 
 fn main() {
     assert_eq!(part_1(parse(include_str!("test.input.txt"))), 64);
     println!("Part 1: {}", part_1(parse(include_str!("input.txt"))));
+
+    assert_eq!(part_2(parse(include_str!("test.input.txt"))), 58);
+    println!("Part 2: {}", part_2(parse(include_str!("input.txt"))));
 }
