ca292a730149073d49cb396f5346a88a3ab03671
Ashley Anderson <aganders3@gmail.com>
2022-12-27 13:21:41 -0500

    Day 20 + clippy and rustfmt

--- day20/puzzle.rs
+++ day20/puzzle.rs
@@ -1,4 +1,3 @@
-use std::convert::TryInto;
 use std::collections::HashMap;
 use std::iter::FromIterator;
 
@@ -11,7 +10,7 @@ fn parse(s: &str) -> (Vec<isize>, HashMap<usize, (usize, usize)>) {
     next.rotate_left(1);
 
     (
-        nums.clone(),
+        nums,
         HashMap::from_iter(idx.into_iter().zip(prev.into_iter().zip(next.into_iter()))),
     )
 }
@@ -25,24 +24,32 @@ struct LL {
 impl LL {
     fn shift(&mut self, i: usize, n: isize) {
         // remove the node
-        let (old_prev, old_next) = self.map.remove(&i).expect("not in list?").clone();
-        let (old_prev_prev, _) = self.map.get(&old_prev).expect("no old prev?").clone();
-        let (_, old_next_next) = self.map.get(&old_next).expect("no old next?").clone();
+        let (old_prev, old_next) = self.map.remove(&i).expect("not in list?");
+        let (old_prev_prev, _) = *self.map.get(&old_prev).expect("no old prev?");
+        let (_, old_next_next) = *self.map.get(&old_next).expect("no old next?");
         self.map.insert(old_prev, (old_prev_prev, old_next));
         self.map.insert(old_next, (old_prev, old_next_next));
 
         // new location
-        let new_prev = if n > 0 {
-            (0..n - 1).fold(old_next, |acc, _v| self.map.get(&acc).expect("can't chase fwd").1)
-        } else if n < 0 {
-            (0..n.abs()).fold(old_prev, |acc, _v| self.map.get(&acc).expect("can't chase rev").0)
-        } else {
-            old_prev
+        let new_prev = match n {
+            n if n < 0 => {
+                let n = n.abs() % (self.len as isize - 1);
+                (0..n).fold(old_prev, |acc, _v| {
+                    self.map.get(&acc).expect("can't chase rev").0
+                })
+            },
+            n if n > 0 => {
+                let n = (n - 1) % (self.len as isize - 1);
+                (0..n).fold(old_next, |acc, _v| {
+                    self.map.get(&acc).expect("can't chase fwd").1
+                })
+            }
+            _ => old_prev,
         };
 
         // reinsert the node
-        let (new_prev_prev, new_next) = self.map.get(&new_prev).expect("no new prev?").clone();
-        let (_, new_next_next) = self.map.get(&new_next).expect("no new next?").clone();
+        let (new_prev_prev, new_next) = *self.map.get(&new_prev).expect("no new prev?");
+        let (_, new_next_next) = *self.map.get(&new_next).expect("no new next?");
         self.map.insert(i, (new_prev, new_next));
         self.map.insert(new_prev, (new_prev_prev, i));
         self.map.insert(new_next, (i, new_next_next));
@@ -51,7 +58,7 @@ impl LL {
 
 fn part_1(nums: &Vec<isize>, map: HashMap<usize, (usize, usize)>) -> isize {
     let mut list = LL {
-        map: map,
+        map,
         len: nums.len(),
     };
     nums.iter().enumerate().for_each(|(i, n)| {
@@ -59,15 +66,30 @@ fn part_1(nums: &Vec<isize>, map: HashMap<usize, (usize, usize)>) -> isize {
     });
 
     let index_of_zero = nums.iter().position(|&x| x == 0).unwrap();
-
-    dbg!(nums[(0..1000).fold(dbg!(index_of_zero), |acc, _v| list.map.get(&acc).expect("can't chase fwd").1)])
-    + dbg!(nums[(0..2000).fold(index_of_zero, |acc, _v| list.map.get(&acc).expect("can't chase fwd").1)])
-    + dbg!(nums[(0..3000).fold(index_of_zero, |acc, _v| list.map.get(&acc).expect("can't chase fwd").1)])
+    dbg!(
+        nums[(0..1000).fold(dbg!(index_of_zero), |acc, _v| list
+            .map
+            .get(&acc)
+            .expect("can't chase fwd")
+            .1)]
+    ) + dbg!(
+        nums[(0..2000).fold(index_of_zero, |acc, _v| list
+            .map
+            .get(&acc)
+            .expect("can't chase fwd")
+            .1)]
+    ) + dbg!(
+        nums[(0..3000).fold(index_of_zero, |acc, _v| list
+            .map
+            .get(&acc)
+            .expect("can't chase fwd")
+            .1)]
+    )
 }
 
 fn part_2(nums: &Vec<isize>, map: HashMap<usize, (usize, usize)>) -> isize {
     let mut list = LL {
-        map: map,
+        map,
         len: nums.len(),
     };
 
@@ -81,14 +103,25 @@ fn part_2(nums: &Vec<isize>, map: HashMap<usize, (usize, usize)>) -> isize {
     }
 
     let index_of_zero = nums.iter().position(|&x| x == 0).unwrap();
-    (0..list.len).for_each(|i| {
-        let i = i + index_of_zero;
-        dbg!(i, nums[i % list.len]);
-    });
-
-    dbg!(nums[(0..1000).fold(dbg!(index_of_zero), |acc, _v| list.map.get(&acc).expect("can't chase fwd").1)])
-    + dbg!(nums[(0..2000).fold(index_of_zero, |acc, _v| list.map.get(&acc).expect("can't chase fwd").1)])
-    + dbg!(nums[(0..3000).fold(index_of_zero, |acc, _v| list.map.get(&acc).expect("can't chase fwd").1)])
+    dbg!(
+        nums[(0..1000).fold(dbg!(index_of_zero), |acc, _v| list
+            .map
+            .get(&acc)
+            .expect("can't chase fwd")
+            .1)]
+    ) + dbg!(
+        nums[(0..2000).fold(index_of_zero, |acc, _v| list
+            .map
+            .get(&acc)
+            .expect("can't chase fwd")
+            .1)]
+    ) + dbg!(
+        nums[(0..3000).fold(index_of_zero, |acc, _v| list
+            .map
+            .get(&acc)
+            .expect("can't chase fwd")
+            .1)]
+    )
 }
 
 fn main() {
@@ -102,8 +135,10 @@ fn main() {
     assert_eq!(part_1_res, 7153);
 
     let (nums, map) = parse(include_str!("test.input.txt"));
-    // assert_eq!(part_2(&nums, map), 1623178306);
+    assert_eq!(part_2(&nums, map), 1623178306);
 
     let (nums, map) = parse(include_str!("input.txt"));
-    // println!("Part 2 {}", part_2(&nums, map));
+    let part_2_res = part_2(&nums, map);
+    println!("Part 2 {}", part_2_res);
+    assert_eq!(part_2_res, 6146976244822);
 }
